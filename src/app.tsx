55e5916a259f0403a6d3894953396daf618ed323
Higor <higorruann0510@gmail.com>
2024-10-10 23:14:20 -0300

    Project structure

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+// App.tsx
+
+import React from 'react';
+import BottomMenu from './components/menu/app';
+
+export default function App() {
+    return (
+        <BottomMenu />
+    );
+}
---
+++ src/components/menu/app.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from '../../pages/home/app';
+import DataUser from '../../pages/dataUser/app';
+
+const Tab = createBottomTabNavigator();
+
+const BottomMenu = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={{
+                    tabBarStyle: { position: 'absolute', bottom: 0, left: 0, right: 0 },
+                }}
+            >
+                <Tab.Screen name="HOME" component={HomeScreen} />
+                <Tab.Screen name="USER" component={DataUser} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default BottomMenu;
