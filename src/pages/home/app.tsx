99335d71f076d1ba9ecea8986f357e54e0372705
Higor <higorruann0510@gmail.com>
2024-10-21 23:32:52 -0300

    Voltei

--- src/pages/home/app.tsx
+++ src/pages/home/app.tsx
@@ -5,7 +5,7 @@ import { View, Text } from 'react-native';
 function HomeScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home user</Text>
+            <Text>Home Screen</Text>
         </View>
     );
 }
